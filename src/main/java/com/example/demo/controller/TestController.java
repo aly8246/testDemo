324e37e7422bab39d690a480ef9f448746c2e66a
gols <31948863+aly8246@users.noreply.github.com>
2020-04-19 19:21:06 +0800

    Update TestController.java

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,15 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("test/")
 public class TestController {
 
+    @Value("server.id")
+    private String id;
+
     @GetMapping
     public String sayHello() {
-        return "hello world";
+        return "hello world instanceId:" + id;
     }
 }
